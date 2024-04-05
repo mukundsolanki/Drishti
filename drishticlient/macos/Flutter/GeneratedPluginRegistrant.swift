3fb5263be29ee364413a5a996faf11f0b92aa8cd
Mukund Solanki <mukundsolanki665@gmail.com>
2024-04-06 01:57:28 +0530

    Updated all the dependencies and Assets used

--- drishticlient/macos/Flutter/GeneratedPluginRegistrant.swift
+++ drishticlient/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import location
+import network_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
 }
